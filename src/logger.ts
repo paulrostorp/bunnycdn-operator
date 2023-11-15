7ec384b79130c8e9089005dacf65e132535e3a5c
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2023-11-15 16:34:10 +0100

    Replaced 'winston-logfmt' by 'winston' package. Fixes #2

--- src/logger.ts
+++ src/logger.ts
@@ -1,3 +1,7 @@
-import { createLogger } from "winston-logfmt";
-
-export const logger = createLogger();
+import winston from "winston";
+const { combine, errors, simple } = winston.format;
+export const logger = winston.createLogger({
+  level: "debug",
+  format: combine(errors({ stack: true }), simple()),
+  transports: [new winston.transports.Console()],
+});
